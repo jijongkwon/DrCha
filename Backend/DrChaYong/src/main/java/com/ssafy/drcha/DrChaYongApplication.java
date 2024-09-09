cd8339220a59adea3c76aba341222e8c0e4c4617
박경림 <pkl4693@naver.com>
2024-09-09 15:59:01 +0900

    init: Backend project 생성

---
+++ Backend/DrChaYong/src/main/java/com/ssafy/drcha/DrChaYongApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.drcha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrChaYongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrChaYongApplication.class, args);
+	}
+
+}
---
+++ Backend/DrChaYong/src/test/java/com/ssafy/drcha/DrChaYongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.drcha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrChaYongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
