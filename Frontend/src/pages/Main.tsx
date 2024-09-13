d331b25b9fab00ba2bd680a5249ce617caf503e4
LEEJW1953 <jiwonlee1953@naver.com>
2024-09-13 15:50:57 +0900

    [S11P21A205-197] feat : 컴포넌트 테스트

--- Frontend/src/pages/Main.tsx
+++ Frontend/src/pages/Main.tsx
@@ -1,8 +1,11 @@
 import { Button } from '@/components/Button/Button';
+import { Input } from '@/components/Input/Input';
+
+import styles from './Main.module.scss';
 
 export function Main() {
   return (
-    <div>
+    <div className={styles.container}>
       <Button color="blue" size="long">
         버튼
       </Button>
@@ -15,6 +18,14 @@ export function Main() {
       <Button color="lightred" size="long">
         버튼
       </Button>
+      <Button color="blue" size="large">
+        버튼
+      </Button>
+      <Button color="blue" size="small">
+        버튼
+      </Button>
+      <Input type="chatting" />
+      <Input type="search" />
     </div>
   );
 }
