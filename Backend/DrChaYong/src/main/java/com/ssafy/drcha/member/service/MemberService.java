1350677070745252ce31bb45405d90e64a8d98af
jijongkwon <help3451@naver.com>
2024-09-20 17:19:02 +0900

    [S11P21A205-213] feat : 맴버 저장 기능 추가

--- Backend/DrChaYong/src/main/java/com/ssafy/drcha/member/service/MemberService.java
+++ Backend/DrChaYong/src/main/java/com/ssafy/drcha/member/service/MemberService.java
@@ -1,9 +1,12 @@
 package com.ssafy.drcha.member.service;
 
+import static com.ssafy.drcha.member.entity.Member.createMember;
+
 import com.ssafy.drcha.global.error.ErrorCode;
 import com.ssafy.drcha.global.error.type.UserNotFoundException;
 import com.ssafy.drcha.member.dto.PhoneNumberRequest;
 import com.ssafy.drcha.member.entity.Member;
+import com.ssafy.drcha.member.enums.MemberRoleStatus;
 import com.ssafy.drcha.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,10 +14,20 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
 public class MemberService {
+
     private final MemberRepository memberRepository;
 
+    @Transactional
+    public Member saveOrUpdateMember(String name, String email, String avatarUrl) {
+        return memberRepository.findByEmail(email)
+                .map(member -> {
+                    member.changeAvatarUrl(avatarUrl);
+                    return memberRepository.save(member);
+                })
+                .orElseGet(() ->
+                        memberRepository.save(createMember(name, email, avatarUrl, MemberRoleStatus.MEMBER))
+                );
     @Transactional(readOnly = true)
     public boolean getVerificationStatusByEmail(String email) {
         Member member = memberRepository.findByEmail(email)
