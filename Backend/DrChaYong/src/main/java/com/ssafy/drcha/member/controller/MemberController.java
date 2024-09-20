fa80fbaac5c24dc778c912c8b37ba122fbdfd2b9
박경림 <pkl4693@naver.com>
2024-09-20 11:08:04 +0900

    [S11P21A205-211] feat : member entity

---
+++ Backend/DrChaYong/src/main/java/com/ssafy/drcha/member/controller/MemberController.java
@@ -0,0 +1,11 @@
+package com.ssafy.drcha.member.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/member")
+public class MemberController {
+
+
+}
--- Backend/DrChaYong/src/main/java/com/ssafy/drcha/member/entity/Member.java
+++ Backend/DrChaYong/src/main/java/com/ssafy/drcha/member/entity/Member.java
@@ -2,18 +2,24 @@ package com.ssafy.drcha.member.entity;
 
 import com.ssafy.drcha.member.enumeration.IsVerified;
 import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "member_id")
-    private int id;
+    private Long id;
 
+    @Column(length = 50)
     private String username;
 
+    @Column(unique = true, length = 100)
     private String email;
 
     @Column(name = "phone_number")
@@ -23,9 +29,11 @@ public class Member {
     @Column(name = "is_verified")
     private IsVerified isVerified;
 
+    @CreatedDate
     @Column(name = "created_at")
     private LocalDateTime createdAt;
 
+    @LastModifiedDate
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 }
