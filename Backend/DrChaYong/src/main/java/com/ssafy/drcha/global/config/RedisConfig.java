aad23b7ce4a49c6f2c9ad3a3b073d2c06ae4a0c6
jijongkwon <help3451@naver.com>
2024-09-20 17:17:53 +0900

    [S11P21A205-213] feat : redis config 설정

---
+++ Backend/DrChaYong/src/main/java/com/ssafy/drcha/global/config/RedisConfig.java
@@ -0,0 +1,105 @@
+package com.ssafy.drcha.global.config;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    private static final String REDISSON_HOST_PREFIX = "redis://";
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    /**
+     * Redisson 클라이언트를 생성하는 빈을 설정
+     *
+     * @return RedissonClient 객체
+     */
+    @Bean
+    public RedissonClient redissonClient() {
+
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress(REDISSON_HOST_PREFIX + redisHost + ":" + redisPort);
+
+        return Redisson.create(config);
+    }
+
+    /**
+     * RedisConnectionFactory 를 생성하는 빈을 설정
+     * Redis 서버에 대한 연결을 관리
+     *
+     * @return RedisConnectionFactory 객체
+     */
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .commandTimeout(Duration.ofSeconds(2))
+                .shutdownTimeout(Duration.ZERO)
+                .build();
+
+        RedisStandaloneConfiguration serverConfig = new RedisStandaloneConfiguration(redisHost, redisPort);
+
+        return new LettuceConnectionFactory(serverConfig, clientConfig);
+    }
+
+    /**
+     * RedisTemplate 을 생성하는 빈을 설정
+     * Redis 와 상호작용하기 위한 템플릿으로, 직렬화 설정을 포함
+     *
+     * @return RedisTemplate 객체
+     */
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate() {
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
+
+    /**
+     * RedisCacheManager 를 생성하는 빈을 설정
+     * 애플리케이션의 캐시 설정을 관리하며, TTL 및 직렬화 방식 등을 설정
+     *
+     * @param redisConnectionFactory RedisConnectionFactory 객체
+     * @return CacheManager 객체
+     */
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofHours(1))
+                .serializeKeysWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(
+                        new GenericJackson2JsonRedisSerializer()));
+
+        Map<String, RedisCacheConfiguration> cacheConfigurations = new HashMap<>();
+        cacheConfigurations.put("outputCache", config);
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(config)
+                .withInitialCacheConfigurations(cacheConfigurations)
+                .build();
+    }
+}
