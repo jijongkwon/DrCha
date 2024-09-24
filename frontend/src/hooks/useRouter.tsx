b628b90c264237422241598155fc82fca35848a7
LEEJW1953 <jiwonlee1953@naver.com>
2024-09-24 17:53:05 +0900

    [S11P21A205-247] feat : router 경로 설정

--- frontend/src/hooks/useRouter.tsx
+++ frontend/src/hooks/useRouter.tsx
@@ -1,6 +1,12 @@
 import { createBrowserRouter } from 'react-router-dom';
 
 import { Layout } from '@/components/Layout/Layout';
+import { Account } from '@/pages/Auth/Account';
+import { AccountSend } from '@/pages/Auth/AccountSend';
+import { Auth } from '@/pages/Auth/Auth';
+import { Complete } from '@/pages/Auth/Complete';
+import { Number } from '@/pages/Auth/Number';
+import { PhoneNumber } from '@/pages/Auth/PhoneNumber';
 import { Login } from '@/pages/Login/Login';
 import { Main } from '@/pages/Main';
 import { Mypage } from '@/pages/Mypage/Mypage';
@@ -24,6 +30,32 @@ export const useRouter = () =>
         {
           path: 'detail',
           element: <TransactionDetail />,
+        },
+        {
+          path: 'auth',
+          element: <Auth />,
+          children: [
+            {
+              path: 'account',
+              element: <Account />,
+            },
+            {
+              path: 'account-send',
+              element: <AccountSend />,
+            },
+            {
+              path: 'number',
+              element: <Number />,
+            },
+            {
+              path: 'phone-number',
+              element: <PhoneNumber />,
+            },
+            {
+              path: 'complete',
+              element: <Complete />,
+            },
+          ],
         },
       ],
     },
