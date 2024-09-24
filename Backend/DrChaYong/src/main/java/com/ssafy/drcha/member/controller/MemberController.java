9226f071ccc179ff7770125ad7413a4ac1efcd64
jijongkwon <help3451@naver.com>
2024-09-24 09:22:24 +0900

    [S11P21A205-227] feat : 로그아웃 기능 구현

--- Backend/DrChaYong/src/main/java/com/ssafy/drcha/member/controller/MemberController.java
+++ Backend/DrChaYong/src/main/java/com/ssafy/drcha/member/controller/MemberController.java
@@ -8,16 +8,23 @@ import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.ErrorResponse;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1/member")
+@RequestMapping("/api/v1/member")
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
@@ -52,5 +59,18 @@ public class MemberController {
     ) {
         memberService.savePhoneNumber(userDetails.getUsername(), phoneNumberRequest);
         return ResponseEntity.ok().build();
+    }
+
+    @Operation(summary = "로그아웃", description = "사용자를 로그아웃하고 인증 토큰을 삭제합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "로그아웃 성공"),
+            @ApiResponse(responseCode = "404", description = "리프레쉬 토큰을 찾을 수 없음",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class),
+                            examples = @ExampleObject(value = "{\"error\": \"Bad Request\", \"message\": \"리프레시 토큰이 없습니다.\"}")))
+    })
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(HttpServletRequest request, HttpServletResponse response) {
+        return new ResponseEntity<>(memberService.logout(request, response), HttpStatus.OK);
     }
 }
