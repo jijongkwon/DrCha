f00943eea7b5f29edc2187d37ae7be9324d23ef5
KimSunWoong <linehero94@gmail.com>
2024-09-27 17:35:48 +0900

    [S11P21A205-284] feat : chat 햄버거 추가

--- frontend/src/pages/Chat/Doctor.tsx
+++ frontend/src/pages/Chat/Doctor.tsx
@@ -9,10 +9,17 @@ export function Doctor() {
         <img src={DOCTOR_IMAGE} alt="doctor" className={styles.doctorprofile} />
         <div className={styles.doctorname}>차용박사</div>
       </div>
-
-      <div className={styles.doctorbubble}>
-        <div className={styles.doctorbubblearrow} />
-        여기에 채팅 내용을 입력하시오.
+      <div className={styles.doctorbubbles}>
+        <div className={styles.doctorbubble}>
+          <div className={styles.doctorbubblearrow} />
+          여기에 채팅 내용을 입력하시오. 여기에 채팅 내용을 입력하시오. 여기에
+          채팅 내용을 입력하시오. 여기에 채팅 내용을 입력하시오. 여기에 채팅
+          내용을 입력하시오.
+        </div>
+        <div className={styles.doctorbubble}>
+          <div className={styles.doctorbubblearrow} />
+          dfdafa
+        </div>
       </div>
     </div>
   );
