7e8a6eb156fcb052895c3955e97062849b19efce
LEEJW1953 <jiwonlee1953@naver.com>
2024-10-05 18:12:17 +0900

    [S11P21A205-340] feat : 채팅 타입 변경

--- frontend/src/types/Chat.ts
+++ frontend/src/types/Chat.ts
@@ -1,11 +1,15 @@
 export type ChatRoom = {
   chatRoomId: number;
   name: string;
-  trustStatus?: string;
   avatarUrl: string;
   contractStatus: 'DRAFTING' | 'ACTIVE' | 'COMPLETED' | 'OVERDUE' | 'CANCELLED';
   lastMessage: string;
   iouAmount: number;
   daysUntilDue: number;
   unreadCount: number;
+  memberTrustInfoResponse: {
+    lateTrades: number;
+    debtTrades: number;
+    message: string;
+  };
 };
