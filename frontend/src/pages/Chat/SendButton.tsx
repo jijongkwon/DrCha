bbfe04aa2b8c51997a78c6d4e586bf8197639bd3
LEEJW1953 <jiwonlee1953@naver.com>
2024-10-06 20:10:19 +0900

    [S11P21A205-367] feat : 채팅 전송 버튼 컴포넌트 수정

--- frontend/src/pages/Chat/SendButton.tsx
+++ frontend/src/pages/Chat/SendButton.tsx
@@ -1,29 +1,15 @@
+import SendMessageSVG from '@/assets/icons/sendMessage.svg?react';
+
+import styles from './Chat.module.scss';
+
 interface SendButtonProps {
   onClick: () => void;
 }
 
 export function SendButton({ onClick }: SendButtonProps) {
   return (
-    <button
-      onClick={onClick}
-      style={{
-        background: 'none',
-        border: 'none',
-        cursor: 'pointer',
-      }}
-    >
-      <svg
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M2.01 21L23 12L2.01 3L2 10L17 12L2 14L2.01 21Z"
-          fill="#007BFF"
-        />
-      </svg>
+    <button onClick={onClick} className={styles.sendButton}>
+      <SendMessageSVG />
     </button>
   );
 }
