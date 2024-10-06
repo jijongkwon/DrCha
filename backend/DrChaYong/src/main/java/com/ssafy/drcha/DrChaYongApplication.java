2a3a14663dafc9609f488a6b9a1ef42ae9b12d98
rkdalstj4505 <rkdalstj4505@naver.com>
2024-10-06 16:48:16 +0900

    [S11P21A205-364] fix : swagger 수정

--- backend/DrChaYong/src/main/java/com/ssafy/drcha/DrChaYongApplication.java
+++ backend/DrChaYong/src/main/java/com/ssafy/drcha/DrChaYongApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-// @OpenAPIDefinition(
-// 	servers = {
-// 		@Server(url = "/", description = "https://j11a205.p.ssafy.io/")
-// 	}
-// )
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+	servers = {
+		@Server(url = "/", description = "https://j11a205.p.ssafy.io/")
+	}
+)
 @SpringBootApplication
 @EnableJpaAuditing
 public class DrChaYongApplication {
