c2c81c04bd97e1fdb7ec94ff31f869b48479e7a1
KimSunWoong <linehero94@gmail.com>
2024-10-07 13:54:58 +0900

    [S11P21A205-284] feat : 로그아웃 구현

--- frontend/src/pages/Mypage/Myinfo.tsx
+++ frontend/src/pages/Mypage/Myinfo.tsx
@@ -1,10 +1,18 @@
+import { useNavigate } from 'react-router-dom';
+
 import styles from '@/pages/Mypage/Mypage.module.scss';
+import { member } from '@/services/member';
 import { Info } from '@/types/Member';
 
 import { Certificated } from './Certificated';
 import { NonCertificated } from './NonCertificated';
 
 export function Myinfo({ myInfos }: { myInfos: Info }) {
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    member.doLogout();
+    navigate('/');
+  };
   return (
     <div className={styles.myinfo}>
       <div className={styles.kakaoinfo}>
@@ -15,6 +23,9 @@ export function Myinfo({ myInfos }: { myInfos: Info }) {
           <div className={styles.kakaonickname}>{myInfos.username}님</div>
           <div className={styles.kakaoemail}>{myInfos.email}</div>
         </div>
+        <button className={styles.logout} onClick={handleLogout}>
+          로그아웃
+        </button>
       </div>
       <div className={styles.accountinfo}>
         {myInfos.verified ? (
