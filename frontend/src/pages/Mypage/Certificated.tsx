87b272d1be5d171e3f22ebf10dee9cdf98dfc248
KimSunWoong <linehero94@gmail.com>
2024-10-11 12:25:20 +0900

    [S11P21A205-284] feat : 새로고침 추가

--- frontend/src/pages/Mypage/Certificated.tsx
+++ frontend/src/pages/Mypage/Certificated.tsx
@@ -1,8 +1,12 @@
 import CertificatedSVG from '@/assets/icons/certificated.svg?react';
+import RefreshSVG from '@/assets/icons/refreshicon.svg?react';
 import styles from '@/pages/Mypage/Mypage.module.scss';
 import { Info } from '@/types/Member';
 
 export function Certificated({ myInfos }: { myInfos: Info }) {
+  const handleRefresh = () => {
+    window.location.reload();
+  };
   const formatCurrency = (amount: number) =>
     `${amount.toLocaleString('ko-KR', {
       maximumFractionDigits: 0,
@@ -15,7 +19,7 @@ export function Certificated({ myInfos }: { myInfos: Info }) {
           <div className={styles.accountnum}>{myInfos.accountNo}</div>
         </div>
         <div className={styles.certokay}>
-          <div>
+          <div className={styles.certokaybtn}>
             <CertificatedSVG fill="#ffffff" />
             <span>본인 인증 완료</span>
           </div>
@@ -23,6 +27,9 @@ export function Certificated({ myInfos }: { myInfos: Info }) {
       </div>
       <div className={styles.balance}>
         <span>{formatCurrency(myInfos.balance)}</span>
+        <div className={styles.refreshbtn} onClick={handleRefresh}>
+          <RefreshSVG />
+        </div>
       </div>
     </div>
   );
